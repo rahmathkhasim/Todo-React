31f916f274b17e3d8ee33fddac7a55378ad0b690
Mohd Rahmath <rahmathmohd1654@gmail.com>
2024-11-11 22:05:08 +0530

    vite file changed

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,7 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
-// https://vite.dev/config/
 export default defineConfig({
+  root: './', // if your index.html is in the root directory
   plugins: [react()],
-})
+});
